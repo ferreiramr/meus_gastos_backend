7c965ca7a2f409596ce3397c808738d2c0719120
Gabriel Ferreira <bi194509@gmail.com>
2022-07-17 13:43:00 -0300

    feature: Novos endpoints

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from fastapi.exceptions import HTTPException
 from fastapi import FastAPI
 from fastapi import status
 from fastapi.responses import JSONResponse
 
-from models import UserSingUp
+from models import User, UserSingUp, UserLogin
 
 app = FastAPI()
+joao = User()
+usuarios = [joao]
 
-@app.post("/login", )
-async def login(user: UserSingUp):
+
+@app.post("/singin")
+async def singin(user: UserSingUp):
     return JSONResponse(
         status_code=status.HTTP_201_CREATED,
         content={
@@ -16,3 +20,58 @@ async def login(user: UserSingUp):
             "status_code": status.HTTP_201_CREATED
         }
     )
+
+@app.post("/login")
+async def login(user: UserLogin):
+    #for na liste de usuarios para ver se ele existe, se tiver certo loga, se naõ retorna erro
+    for usuario in usuarios:
+        if usuario.login == user.login:
+            if usuario.password != user.password:
+                raise HTTPException(
+                    status_code = status.HTTP_401_UNAUTHORIZED,
+                    detail="Usuário ou senha incorretos"
+                )
+            return usuario
+    raise HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail="Usuário ou senha incorretos"
+    )
+    
+
+# @app.post("/patrimonio")
+# async def getUser(user: User):
+#     return JSONResponse(
+#         status_code=status.HTTP_200_OK,
+#         content={
+#             "message": "Usuário logado"
+#         }
+#     )
+
+@app.get("/usuario/{id}")
+async def getUsuario(id: int):
+    usuario = usuarios[id]
+    return usuario
+
+@app.get("/add_gastos/{id}")
+async def add_gastos(gasto: float, id: int):
+    usuario = usuarios[id]
+    usuario.gastos += gasto
+    usuario.patrimonio -= gasto
+    return usuario
+
+@app.get("/add_ganhos/{id}")
+async def add_ganhos(ganhos: float, id: int):
+    usuario = usuarios[id]
+    usuario.ganhos += ganhos
+    usuario.patrimonio += ganhos
+    return usuario
+
+@app.post("/create_user")
+async def create_user(usuario: User):
+    usuario.id = len(usuarios) - 1
+    usuarios.append(usuario)
+    return usuario
+
+@app.get("/users")
+async def get_users():
+    return usuarios
--- models.py
+++ models.py
@@ -1,8 +1,22 @@
 from pydantic import BaseModel
 
 
+
 class UserSingUp(BaseModel):
     login: str
     email: str
     password: str
     password_confirmation: str
+
+class UserLogin(BaseModel):
+    login: str
+    password: str
+
+class User(BaseModel):
+    id: int = 0
+    login: str = "joao"
+    password: str = "123456"
+    nome: str = "João Pé de Feijão"
+    patrimonio: float = 1500
+    gastos: float = 0
+    ganhos: float = 0
